a4ca52ba96e44020fdb9b53e920ceaf36b772cd6
Victor Shcherbatyuk <Victor.Shcherbatyuk3@ibm.com>
2022-05-18 13:36:17 +0300

    Making LDAP BIND USER configurable, not hardcoded

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,15 +5,17 @@ set -euo pipefail
 if [ "$1" = 'afp-web' ]; then
 
 	echo "Copy Airflow Metastore Connection String to Airflow conf"
-	sed -i "s|changemestring|${AIRFLOW__CORE__SQL_ALCHEMY_CONN}|" ${AIRFLOW_HOME}/airflow.cfg
+	sed -i "s|connectionstr|${AIRFLOW__CORE__SQL_ALCHEMY_CONN}|" ${AIRFLOW_HOME}/airflow.cfg
 	echo "Copy Airflow Fernet Key to Airflow conf"
-	sed -i "s/changemekey/${AIRFLOW__CORE__FERNET_KEY}/" ${AIRFLOW_HOME}/airflow.cfg
+	sed -i "s/fernetkey/${AIRFLOW__CORE__FERNET_KEY}/" ${AIRFLOW_HOME}/airflow.cfg
 	echo "Copy Base URL to Airflow conf"
-	sed -i "s|changemeurl|${BASE_URL}|" ${AIRFLOW_HOME}/airflow.cfg
+	sed -i "s|baseurl|${BASE_URL}|" ${AIRFLOW_HOME}/airflow.cfg
 	echo "Copy StatsD prefix to Airflow conf"
-	sed -i "s/changemeprefix/${STATSD_PREFIX}/" ${AIRFLOW_HOME}/airflow.cfg
+	sed -i "s/statsdprefix/${STATSD_PREFIX}/" ${AIRFLOW_HOME}/airflow.cfg
+	echo "Copy LDAP Bind User to webserver_config.py"
+	sed -i "s/ldapbinduser/${LDAP_BIND_USER}/" ${AIRFLOW_HOME}/webserver_config.py
 	echo "Copy LDAP Bind Password to webserver_config.py"
-	sed -i "s/changeme/${LDAP_BIND_PASSWORD}/" ${AIRFLOW_HOME}/webserver_config.py
+	sed -i "s/ldapbindpassword/${LDAP_BIND_PASSWORD}/" ${AIRFLOW_HOME}/webserver_config.py
 	
 	echo "Copy DAGs from GIT"
 	git clone --single-branch --branch ${BRANCH} https://${GIT_ACCESS_TOKEN}@github.ibm.com/CIO-MAP/MAP-ETL-Framework ${AIRFLOW_GIT}
@@ -37,13 +39,13 @@ if [ "$1" = 'afp-web' ]; then
 elif [ "$1" = 'afp-sched' ]; then
 	
 	echo "Copy Airflow Metastore Connection String to Airflow conf"
-	sed -i "s|changemestring|${AIRFLOW__CORE__SQL_ALCHEMY_CONN}|" ${AIRFLOW_HOME}/airflow.cfg
+	sed -i "s|connectionstr|${AIRFLOW__CORE__SQL_ALCHEMY_CONN}|" ${AIRFLOW_HOME}/airflow.cfg
 	echo "Copy Airflow Fernet Key to Airflow conf"
-	sed -i "s/changemekey/${AIRFLOW__CORE__FERNET_KEY}/" ${AIRFLOW_HOME}/airflow.cfg
+	sed -i "s/fernetkey/${AIRFLOW__CORE__FERNET_KEY}/" ${AIRFLOW_HOME}/airflow.cfg
 	echo "Copy Base URL to Airflow conf"
-	sed -i "s|changemeurl|${BASE_URL}|" ${AIRFLOW_HOME}/airflow.cfg
+	sed -i "s|baseurl|${BASE_URL}|" ${AIRFLOW_HOME}/airflow.cfg
 	echo "Copy StatsD prefix to Airflow conf"
-	sed -i "s/changemeprefix/${STATSD_PREFIX}/" ${AIRFLOW_HOME}/airflow.cfg
+	sed -i "s/statsdprefix/${STATSD_PREFIX}/" ${AIRFLOW_HOME}/airflow.cfg
 	
 	echo "Copy DAGs from GIT"
 	git clone --single-branch --branch ${BRANCH} https://${GIT_ACCESS_TOKEN}@github.ibm.com/CIO-MAP/MAP-ETL-Framework ${AIRFLOW_GIT}
